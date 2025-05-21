a6a519a6a1b6a79e0c3868ed22f0dbc67eb32a37
Marcos Henrique <marcosriderbr@outlook.com>
2025-05-21 16:06:46 -0300

    - Implementado calculo para o IMC, além da lógica para obter os dados e enviar para a tela Result.

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculadoraimc
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnCalcular: Button
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +21,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnCalcular = findViewById(R.id.btnCalcular)
+        editWeight = findViewById(R.id.editWeight)
+        editHeight = findViewById(R.id.editHeight)
+
         btnCalcular.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
+
+            val weight = editWeight.text.toString()
+            val height = editHeight.text.toString()
+
+            if(weight.isEmpty()){
+                editWeight.error = "Peso é obrigatório!"
+                return@setOnClickListener
+            }
+            if(height.isEmpty()){
+                editHeight.error = "Altura é obrigatória!"
+                return@setOnClickListener
+            }
+
+            intent.putExtra("weight", weight.toDouble())
+            intent.putExtra("height", height.toDouble())
+
             startActivity(intent)
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- app/src/main/java/com/example/calculadoraimc/ResultActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/ResultActivity.kt
@@ -1,16 +1,45 @@
 package com.example.calculadoraimc
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var textWeight: TextView
+    private lateinit var textHeight: TextView
+    private lateinit var textResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
+
+        textWeight = findViewById(R.id.textWeight)
+        textHeight = findViewById(R.id.textHeight)
+        textResult = findViewById(R.id.textResult)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val weight = bundle.getDouble("weight")
+            val height = bundle.getDouble("height") / 100
+
+            textWeight.text = "Peso informado: %.2f Kg".format(weight)
+            textHeight.text = "Altura informada: %.2f m".format(height)
+
+            val imc = weight / (height * height)
+
+            textResult.text = when {
+                imc in 18.5..24.9 -> "Seu IMC é %.2f. Você está no peso normal.".format(imc)
+                imc in 25.0..29.9 -> "Seu IMC é %.2f. Você está com sobrepeso.".format(imc)
+                imc in 30.0..34.9 -> "Seu IMC é %.2f. Você está com obesidade.".format(imc)
+                else -> "Seu IMC é %.2f. Você está com obesidade grave.".format(imc)
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
